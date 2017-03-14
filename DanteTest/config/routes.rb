cb44fd8062f8d94d19e63ad6a72e17a5304108cc
Cassidy Haas <cassidyhhaas@gmail.com>
2017-03-13 20:14:44 -0600

    update routs.rb to go to homepage instead of user page

--- DanteTest/config/routes.rb
+++ DanteTest/config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-get 'test' => 'pages#test'
+get 'test' => 'pages#homepage'
 get 'anothertest' => 'pages#anothertest'
 end
