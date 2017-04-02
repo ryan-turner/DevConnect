c1a178e9bd2a1bb1526fca64f6aa257e91eeafe4
jeffreylipnick <lipnick@colorado.edu>
2017-04-02 17:01:46 -0600

    Added Groups Table. Set up start of matching algorithm.

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @matches = User.matches
   end
 
   # GET /users/new
--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+	def self.matches
+		@matches = Group.all
+		@matches = @matches.to_json
+	end
+
 end
