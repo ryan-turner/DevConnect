14879e9439977d1a5a808f4a5c39bdf07e742f02
Cassidy Haas <cassidyhhaas@gmail.com>
2017-04-25 19:05:58 -0600

    commiting changes to stylesheet

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Forces rails to serve static assets
+  config.serve_static_assets = true
 end
